4db2e2674fbf0116d50027a36be54369d6eeb5db
Glenn Mizuno <turbo159@outlook.com>
2019-07-05 23:15:10 -0500

    blah

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-from flask import Flask
-from redis import Redis, RedisError
 from urllib2 import Request, urlopen, URLError
 import os
 import socket
 
 # Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
-
-app = Flask(__name__)
 
 @app.route("/")
 def hello():
