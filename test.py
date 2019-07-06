986bd4757b1ff91acac825f137d0cbc4d0415530
Glenn Mizuno <turbo159@outlook.com>
2019-07-05 20:50:15 -0500

    HelloWorld

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from urllib2 import Request, urlopen, URLError
 
-request = Request('https://itunes.apple.com/search?term=simple minds')
+# request = Request('https://itunes.apple.com/search?term=simple minds')
+request = Request('https://servicesqa.na.cargill.com:8443/core/request/info?hello=there')
 
 try:
 	response = urlopen(request)
